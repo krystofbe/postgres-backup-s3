0896fb389b830ff6d2ee75457c1abc37cfbaca8b
Krystof Beuermann <krystof@blackbox.ms>
2023-12-29 13:31:19 +0100

    add POSTGRES_BACKUP_ALL option

--- src/backup.sh
+++ src/backup.sh
@@ -5,27 +5,36 @@ set -o pipefail
 
 source ./env.sh
 
-echo "Creating backup of $POSTGRES_DATABASE database..."
-pg_dump --format=custom \
-        -h $POSTGRES_HOST \
-        -p $POSTGRES_PORT \
-        -U $POSTGRES_USER \
-        -d $POSTGRES_DATABASE \
-        $PGDUMP_EXTRA_OPTS \
-        > db.dump
-
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+
+# Check if POSTGRES_BACKUP_ALL is set to "true"
+if [ "${POSTGRES_BACKUP_ALL}" = "true" ]; then
+  echo "Creating dump of all databases..."
+  SRC_FILE="alldb_${timestamp}.dump"
+  DEST_FILE="postgres_${timestamp}.dump"
+
+  # Dump all databases
+  pg_dumpall -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" $PGDUMP_EXTRA_OPTS >"$SRC_FILE"
+else
+  echo "Creating backup of $POSTGRES_DATABASE database..."
+  SRC_FILE="db_${timestamp}.dump"
+  DEST_FILE="${POSTGRES_DATABASE}_${timestamp}.dump"
+
+  # Dump the specified database
+  pg_dump --format=custom -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE" $PGDUMP_EXTRA_OPTS >"$SRC_FILE"
+fi
+
+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${DEST_FILE}"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
-  rm -f db.dump.gpg
-  gpg --symmetric --batch --passphrase "$PASSPHRASE" db.dump
-  rm db.dump
-  local_file="db.dump.gpg"
+  rm -f "${SRC_FILE}.gpg"
+  gpg --symmetric --batch --passphrase "$PASSPHRASE" "$SRC_FILE"
+  rm "$SRC_FILE"
+  local_file="${SRC_FILE}.gpg"
   s3_uri="${s3_uri_base}.gpg"
 else
-  local_file="db.dump"
+  local_file="$SRC_FILE"
   s3_uri="$s3_uri_base"
 fi
 
@@ -36,7 +45,7 @@ rm "$local_file"
 echo "Backup complete."
 
 if [ -n "$BACKUP_KEEP_DAYS" ]; then
-  sec=$((86400*BACKUP_KEEP_DAYS))
+  sec=$((86400 * BACKUP_KEEP_DAYS))
   date_from_remove=$(date -d "@$(($(date +%s) - sec))" +%Y-%m-%d)
   backups_query="Contents[?LastModified<='${date_from_remove} 00:00:00'].{Key: Key}"
 
@@ -45,7 +54,7 @@ if [ -n "$BACKUP_KEEP_DAYS" ]; then
     --bucket "${S3_BUCKET}" \
     --prefix "${S3_PREFIX}" \
     --query "${backups_query}" \
-    --output text \
-    | xargs -n1 -t -I 'KEY' aws $aws_args s3 rm s3://"${S3_BUCKET}"/'KEY'
+    --output text |
+    xargs -n1 -t -I 'KEY' aws $aws_args s3 rm s3://"${S3_BUCKET}"/'KEY'
   echo "Removal complete."
 fi
--- src/restore.sh
+++ src/restore.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-set -u # `-e` omitted intentionally, but i can't remember why exactly :'(
+set -u # `-e` omitted intentionally
 set -o pipefail
 
 source ./env.sh
@@ -15,14 +15,21 @@ fi
 
 if [ $# -eq 1 ]; then
   timestamp="$1"
-  key_suffix="${POSTGRES_DATABASE}_${timestamp}${file_type}"
+  # Adjust to allow for restoring all databases if POSTGRES_BACKUP_ALL is set
+  if [ "${POSTGRES_BACKUP_ALL}" = "true" ]; then
+    key_suffix="postgres_${timestamp}${file_type}"
+  else
+    key_suffix="${POSTGRES_DATABASE}_${timestamp}${file_type}"
+  fi
 else
   echo "Finding latest backup..."
+  # Adjust the search to consider all databases backup
   key_suffix=$(
-    aws $aws_args s3 ls "${s3_uri_base}/${POSTGRES_DATABASE}" \
-      | sort \
-      | tail -n 1 \
-      | awk '{ print $4 }'
+    aws $aws_args s3 ls "${s3_uri_base}/" |
+      grep -E "(postgres_|${POSTGRES_DATABASE}_).*${file_type}$" |
+      sort |
+      tail -n 1 |
+      awk '{ print $4 }'
   )
 fi
 
@@ -31,14 +38,20 @@ aws $aws_args s3 cp "${s3_uri_base}/${key_suffix}" "db${file_type}"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Decrypting backup..."
-  gpg --decrypt --batch --passphrase "$PASSPHRASE" db.dump.gpg > db.dump
-  rm db.dump.gpg
+  gpg --decrypt --batch --passphrase "$PASSPHRASE" "db${file_type}" >db.dump
+  rm "db${file_type}"
 fi
 
-conn_opts="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE"
+# Adjust restoration process based on backup type
+if [ "${POSTGRES_BACKUP_ALL}" = "true" ]; then
+  echo "Restoring all databases from backup..."
+  psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -f db.dump
+else
+  conn_opts="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE"
+  echo "Restoring from backup..."
+  pg_restore $conn_opts --clean --if-exists db.dump
+fi
 
-echo "Restoring from backup..."
-pg_restore $conn_opts --clean --if-exists db.dump
 rm db.dump
 
 echo "Restore complete."
