c6fd3b7dac12640f2bf9cce675e8daa218cb8e5b
Krystof Beuermann <krystof@blackbox.ms>
2023-12-29 14:27:27 +0100

    use alpine cron

--- src/install.sh
+++ src/install.sh
@@ -14,12 +14,5 @@ apk add gnupg
 # install aws-cli
 apk add aws-cli
 
-# install go-crond
-GOCROND_VERSION=22.9.1
-GOCRON_OS=linux
-GOCRON_ARCH=amd64
-wget -O /usr/local/bin/go-crond "https://github.com/webdevops/go-crond/releases/download/${GOCROND_VERSION}/go-crond.${GOCRON_OS}.${GOCRON_ARCH}"
-chmod +x /usr/local/bin/go-crond
-
 # cleanup
 rm -rf /var/cache/apk/*
--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,20 @@
-#! /bin/sh
+#!/bin/sh
 
 set -eu
 
+# Configure AWS S3 if required
 if [ "$S3_S3V4" = "yes" ]; then
   aws configure set default.s3.signature_version s3v4
 fi
 
+# Check if SCHEDULE is set
 if [ -z "$SCHEDULE" ]; then
+  # Run backup script immediately if no schedule is set
   sh backup.sh
 else
-  exec go-cron "$SCHEDULE" /bin/sh backup.sh
+  # Add the backup script to the crontab
+  echo "$SCHEDULE /bin/sh /backup.sh" >/etc/crontabs/root
+
+  # Start the cron daemon
+  crond -f -l 8
 fi
